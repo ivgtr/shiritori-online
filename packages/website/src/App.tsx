27e14d3b0364869d4b5647eec4a89635876c6fc6
ivgtr <ivgtr.me@gmail.com>
2021-07-09 16:03:28 +0900

    :sparkles: add: 通信周りをswrに任せよう

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -8,7 +8,9 @@ export const App = () => {
   return (
     <Container>
       <PageHeader />
+
       <PageContents />
+
       <PageFooter />
     </Container>
   );
--- packages/website/src/components/PageContents/index.tsx
+++ packages/website/src/components/PageContents/index.tsx
@@ -1,13 +1,18 @@
 import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef } from "react";
+import useSWR, { mutate } from "swr";
 import { v4 as uuid } from "uuid";
+import { Loader } from "../Loader";
 
-type Shiritori = { word: string; user: string; words: string[] };
+export type Shiritori = { word: string; user: string; words: string[] };
+type JudgeResult = { result: boolean; text: string };
+
+const API_URL = "https://shiritori-api.glitch.me/shiritori";
 
 const judgeShiritori = (answer: { word: string; user: string }) =>
   axios
     .post(
-      "https://shiritori-api.glitch.me/shiritori",
+      API_URL,
       { shiritori: JSON.stringify(answer) },
       {
         headers: {
@@ -15,83 +20,78 @@ const judgeShiritori = (answer: { word: string; user: string }) =>
         },
       }
     )
-    .then<{ result: boolean; text: string }>((result) => result.data);
+    .then<JudgeResult>((result) => result.data);
 
-export const PageContents = () => {
-  const [shiritori, setShiritori] = useState<Shiritori>({ word: "", user: "", words: [] });
+const getShititori = (url: string) => {
+  return axios.get(url).then<Shiritori>((result) => result.data);
+};
+
+export const PageContents: React.VFC = () => {
   const inputRef = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    (async () => {
-      const data = await axios
-        .get("https://shiritori-api.glitch.me/shiritori")
-        .then<Shiritori>((result) => result.data);
+  const { data, error } = useSWR(API_URL, getShititori);
 
-      setShiritori(data);
-    })();
-  }, []);
+  if (error) return <div>failed to load</div>;
+  if (!data) return <Loader />;
 
   return (
     <main>
-      {shiritori.word && (
-        <>
-          <section className="max-w-lg mx-auto mt-4">
-            <h3>既に出た単語</h3>
-            <div>
-              <textarea
-                defaultValue={shiritori.words}
-                readOnly
-                rows={10}
-                className="w-full p-2 resize-y cursor-default"
-              ></textarea>
-            </div>
-          </section>
-          <section className="max-w-lg mx-auto mt-4">
-            <h3>現在の単語</h3>
-            <div>
-              <p>{shiritori.word}</p>
-            </div>
-          </section>
-          <section className="max-w-lg mx-auto mt-4">
-            <h3>入力</h3>
-            <div>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
+      <section className="max-w-lg mx-auto mt-4">
+        <h3>既に出た単語</h3>
+        <div>
+          <textarea
+            defaultValue={data.words.join("→")}
+            readOnly
+            rows={10}
+            className="w-full p-2 resize-y cursor-default"
+          ></textarea>
+        </div>
+      </section>
+      <section className="max-w-lg mx-auto mt-4">
+        <h3>現在の単語</h3>
+        <div>
+          <p>{data.word}</p>
+        </div>
+      </section>
+      <section className="max-w-lg mx-auto mt-4">
+        <h3>入力</h3>
+        <div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
 
-                  const input = inputRef.current;
-                  if (!input?.value) return;
-                  const answer = {
-                    word: input.value,
-                    user: uuid(),
-                  };
-                  judgeShiritori(answer).then((judge) => {
-                    if (judge.result) {
-                      alert(`正解: '${input.value}'を回答しました！`);
-                    } else {
-                      alert(`${"(´- ₃ -`) ﾌﾞﾌﾞｰ"} '${input.value}'は間違いです、${judge.text}`);
-                    }
-                  });
-                }}
-              >
-                <div className="w-full flex gap-2">
-                  <input
-                    ref={inputRef}
-                    placeholder={"入力してね"}
-                    defaultValue={shiritori.word.slice(-1)}
-                    type="text"
-                    className="w-full p-2 box-border border border-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md"
-                  />
-                  <button className="p-2 border border-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md whitespace-nowrap">
-                    <span className="inline-block text-gray-500 text-base h-4 leading-4 align-middle">
-                      答える
-                    </span>
-                  </button>
-                </div>
-              </form>
+              const input = inputRef.current;
+              if (!input?.value) return;
+              const answer = {
+                word: input.value,
+                user: uuid(),
+              };
+              const judge = judgeShiritori(answer).then((judge) => {
+                if (judge.result) {
+                  alert(`正解: '${input.value}'を回答しました！`);
+                } else {
+                  alert(`${"(´- ₃ -`) ﾌﾞﾌﾞｰ"} '${input.value}'は間違いです、${judge.text}`);
+                }
+                input.value = "";
+              });
+              mutate(API_URL, judge);
+            }}
+          >
+            <div className="w-full flex gap-2">
+              <input
+                ref={inputRef}
+                placeholder={"入力してね"}
+                type="text"
+                className="w-full p-2 box-border border border-gray-500 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md"
+              />
+              <button className="p-2 border border-gray-500 shadow-sm hover:border-indigo-300 hover:ring hover:ring-indigo-200 hover:ring-opacity-50 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md whitespace-nowrap">
+                <span className="inline-block text-gray-500 text-base h-4 leading-4 align-middle hover:text-indigo-300">
+                  答える
+                </span>
+              </button>
             </div>
-          </section>
-        </>
-      )}
+          </form>
+        </div>
+      </section>
     </main>
   );
 };
